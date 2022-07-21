98471fcc8e8ff671dcc1e8f8f12b67f81a301f71
davidlechuga <lechugazteca@gmail.com>
2022-07-21 12:56:22 -0400

    new version

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,11 +3,29 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:e68a5ee8-5387-463c-a3f7-f65142d18aa4",
+    "aws_cognito_identity_pool_id": "us-east-1:b5541e67-68df-4ea8-9068-94ce537b7eb7",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_Iht6rnGlO",
-    "aws_user_pools_web_client_id": "41h9uvqrq8ighn9lfh7shln0t9",
-    "oauth": {}
+    "aws_user_pools_id": "us-east-1_DXEHWscfa",
+    "aws_user_pools_web_client_id": "rkp30k24ukdkpinc4eq42l4o6",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "PHONE_NUMBER"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
 };
 
 
